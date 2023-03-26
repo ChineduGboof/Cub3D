28842404bdb53a57dfc628249685833108171db3
Chinedu Egbulefu <egbulefuchinedu@gmail.com>
2023-03-26 04:42:17 +0400

    added back the image file

--- cub3d/source/main.c
+++ cub3d/source/main.c
@@ -6,25 +6,24 @@
 /*   By: gboof <gboof@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 14:15:33 by oaydemir          #+#    #+#             */
-/*   Updated: 2023/03/26 03:58:52 by gboof            ###   ########.fr       */
+/*   Updated: 2023/03/26 04:34:54 by gboof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// t_specifications	fake_parse(char *cub_file)
-// {
-// 	t_specifications	specifications = { 0 };
+t_specifications	fake_parse(char *cub_file)
+{
+	t_specifications	specifications = { 0 };
 	
-// 	return (specifications);
-// }
+	return (specifications);
+}
 	
-
 // void init_str
 
 int	main(int argc, char **argv)
 {
-	// t_specifications	specifications;
+	t_specifications	specifications;
 	
 	if (argc != 2)
 	{
@@ -37,9 +36,10 @@ int	main(int argc, char **argv)
 	validate_map_file(argv[1]);
 	// check_map_file_ext checks for valid file extension
 	check_map_file_ext(argv[1]);
-	validate_map(argv[1]);
+	// validate_map(argv[1]);
 	// specifications = fake_parse(argv[1]);
 	// init_struct(&specifications);
 	// run_game(specifications);
 	(void)argv;
+	return (0);
 }
