34c67a3d3f4494b4e59a16df485effa9e2cac708
user8674 <62568969+user8674@users.noreply.github.com>
2023-03-26 20:10:25 +0400

    Update safe_termination.c

--- cub3d/source/safe_termination.c
+++ cub3d/source/safe_termination.c
@@ -6,7 +6,7 @@
 /*   By: oaydemir <oaydemir@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 18:16:10 by oaydemir          #+#    #+#             */
-/*   Updated: 2023/03/26 20:08:31 by oaydemir         ###   ########.fr       */
+/*   Updated: 2023/03/26 20:10:09 by oaydemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	safely_terminate(t_game *game)
 	mlx_destroy_image(game->s_mlx.mlx, game->s_mlx.image->image);
 	mlx_destroy_window(game->s_mlx.mlx, game->s_mlx.window);
 	free(game->s_mlx.image);
+	free(game->s_mlx.mlx);
+
 	// type-agnostic cautious ultimate mega fancy free_2d_array
 	// function from my libft :)
 	// needs indication of map size or null-termination to
