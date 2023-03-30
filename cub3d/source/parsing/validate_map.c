1e2c2c07b93d2225eaabc08d83352c56db049d2d
Chinedu Egbulefu <egbulefuchinedu@gmail.com>
2023-03-30 20:19:01 +0400

    checking the map fence

--- cub3d/source/parsing/validate_map.c
+++ cub3d/source/parsing/validate_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validate_map.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cegbulef <cegbulef@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gboof <gboof@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 23:02:07 by gboof             #+#    #+#             */
-/*   Updated: 2023/03/30 20:09:28 by cegbulef         ###   ########.fr       */
+/*   Updated: 2023/03/30 20:18:33 by gboof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,51 +57,47 @@ bool is_walled(int i, int j, char **map)
 }
 
 bool is_fenced(char **map) {
-    bool first_row_ok = true;
-    bool last_row_ok = true;
-    
-    // Check first row
-    int j = 0;
-    while (map[0][j] != '\0') {
-        if (map[0][j] != '1' && map[0][j] != ' ') {
-            first_row_ok = false;
-            break;
-        }
-        j++;
-    }
-    
-    // Check last row
-    j = 0;
-    while (map[1][j] != '\0') {
-        if (map[1][j] != '1' && map[1][j] != ' ') {
-            last_row_ok = false;
-            break;
-        }
-        j++;
-    }
-    
-    // Check other rows
-    int i = 2;
-    while (map[i] != NULL) {
-        j = 0;
-        while (map[i][j] != '\0') {
-            if (j == 0) {
-                if (map[i][j] != '1' && map[i][j] != ' ') {
-                    return false;
-                }
-            }
-            else if (map[i][j+1] == '\0') {
-                if (map[i][j] != '1') {
-                    return false;
-                }
-            }
-            j++;
+    // Check the first row
+    char *first_row = map[0];
+    int i = 0;
+    while (first_row[i] != '\0') {
+        if (first_row[i] != '1' && first_row[i] != ' ') {
+            return false;
         }
         i++;
     }
     
-    return first_row_ok && last_row_ok;
+    // Check the last row
+    char *last_row = map[0];
+    int num_rows = 0;
+    while (map[num_rows] != NULL) {
+        last_row = map[num_rows];
+        num_rows++;
+    }
+    i = 0;
+    while (last_row[i] != '\0') {
+        if (last_row[i] != '1' && last_row[i] != ' ') {
+            return false;
+        }
+        i++;
+    }
+    
+    // Check the other rows
+    char *current_row = map[1];
+    while (current_row != NULL && current_row != last_row) {
+        if (current_row[0] != '1' && current_row[0] != ' ') {
+            return false;
+        }
+        int len = ft_strlen(current_row);
+        if (current_row[len-1] != '1') {
+            return false;
+        }
+        current_row = *(++map);
+    }
+    
+    return true;
 }
+
 
 void check_map_errors(char **map)
 {
