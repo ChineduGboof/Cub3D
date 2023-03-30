ef52d0f94820d8412cf2b9391207975309eafd6b
Chinedu Egbulefu <egbulefuchinedu@gmail.com>
2023-03-30 07:48:21 +0400

    Fixed the map copying space issue

--- cub3d/source/parsing/parse_map.c
+++ cub3d/source/parsing/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: gboof <gboof@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 19:53:52 by cegbulef          #+#    #+#             */
-/*   Updated: 2023/03/29 23:03:30 by gboof            ###   ########.fr       */
+/*   Updated: 2023/03/30 07:47:12 by gboof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@ void	free_map(t_specifications *specifications, int status)
 		ft_free_2d_array((void ***)&specifications->map, -1, true);
 	exit(status);
 }
+
 void parse_map(const char *map_file_path, t_specifications *specifications)
 {
     int fd;
     int res;
     char *line;
-    int map_size = 4096; // initial size of specifications->map array
-    int map_index = 0; // current index to insert line in specifications->map
+    int map_size = 4096;
+    int map_index = 0;
 
     line = NULL;
     fd = open(map_file_path, O_RDONLY);
@@ -34,14 +35,15 @@ void parse_map(const char *map_file_path, t_specifications *specifications)
         perror("Error: could not open map file");
         exit(EXIT_FAILURE);
     }
-
-    // allocate initial specifications->map array
     specifications->map = (char **)malloc(sizeof(char *) * map_size);
     if (specifications->map == NULL)
         free_map(specifications, EXIT_FAILURE);
-
-    while ((res = get_line(fd, &line)) > 0)
+        
+    while (1)
     {
+        res = get_line(fd, &line);
+        if (res <= 0)
+			break;
         if (*line == '\0')
             continue;
         else if (ft_strnstr(line, NO, 3) == line ||
@@ -56,25 +58,9 @@ void parse_map(const char *map_file_path, t_specifications *specifications)
         }
         else
         {
-            // Replace all tabs with 4 spaces
             char *new_line = ft_strdup(line);
             if (new_line == NULL)
                 free_map(specifications, EXIT_FAILURE);
-
-            char *ptr = new_line;
-            while ((ptr = ft_strchr(ptr, '\t')) != NULL)
-            {
-                // Move pointer forward by 1 tab
-                ptr++;
-
-                // Calculate the number of spaces required to replace the tab
-                int num_spaces = 1 - (ptr - new_line) % 4;
-
-                // Replace the tab with spaces
-                ft_memmove(ptr + num_spaces - 1, ptr, ft_strlen(ptr) + 1);
-                ft_memset(ptr, ' ', num_spaces);
-            }
-
             // expand specifications->map if necessary
             if (map_index >= map_size)
             {
@@ -90,11 +76,11 @@ void parse_map(const char *map_file_path, t_specifications *specifications)
             map_index++;
         }
     }
-
     if (res == -1)
         free_map(specifications, EXIT_FAILURE);
     close(fd);
     check_map_errors(specifications->map);
+
     // print content of map
     int k = 0;
     while (specifications->map[k])
