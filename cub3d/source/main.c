cb06d81da63f5c4d925206b29da22d77c3fb0a3a
Chinedu Egbulefu <egbulefuchinedu@gmail.com>
2023-04-02 05:15:44 +0400

    push

--- cub3d/source/main.c
+++ cub3d/source/main.c
@@ -6,11 +6,16 @@
 /*   By: cegbulef <cegbulef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 14:15:33 by oaydemir          #+#    #+#             */
-/*   Updated: 2023/04/02 04:56:53 by cegbulef         ###   ########.fr       */
+/*   Updated: 2023/04/02 05:05:55 by cegbulef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+bool	wrong_placement(char *file)
+{
+
+}
 
 int	main(int argc, char **argv)
 {
@@ -22,7 +27,7 @@ int	main(int argc, char **argv)
 		return (EXIT_FAILURE);
 	}
 	validate_argument(argv[1]);
-	if (is_empty_file(argv[1]))
+	if (is_empty_file(argv[1]) || wrong_placement(argv[1]))
 	{
 		ft_putstr_fd("Error: map has missing specification(s)\n", STDERR_FILENO);
 		return (EXIT_FAILURE);
