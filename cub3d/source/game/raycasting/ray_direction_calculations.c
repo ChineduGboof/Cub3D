31fde08c121eddaf272ac4f07d0cfce8a7107504
user8674 <62568969+user8674@users.noreply.github.com>
2023-04-03 20:11:41 +0400

    single sneaky norm error from my latest change fixed

--- cub3d/source/game/raycasting/ray_direction_calculations.c
+++ cub3d/source/game/raycasting/ray_direction_calculations.c
@@ -6,7 +6,7 @@
 /*   By: oaydemir <oaydemir@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 13:57:37 by oaydemir          #+#    #+#             */
-/*   Updated: 2023/04/03 19:44:03 by oaydemir         ###   ########.fr       */
+/*   Updated: 2023/04/03 20:10:48 by oaydemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_vector	calculate_ray_direction(t_player player, double offset_from_center)
 	t_vector	ray_direction;
 
 	ray_direction.x = -(player.direction.x
-		+ (player.plane.x * offset_from_center));
+			+ (player.plane.x * offset_from_center));
 	ray_direction.y = player.direction.y
 		+ (player.plane.y * offset_from_center);
 	return (ray_direction);
